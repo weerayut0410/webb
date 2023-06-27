f4ee728c45a0340e7464183a49029594904288f4
weerayut0410 <weerayut.la.65@ubu.ac.th>
2023-06-27 15:27:48 +0700

    asd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
+
+function App() {
+  return (
+    <>
+      <div className='card'>
+        <Avatar />
+        <dvi className = 'data'>
+          <Intro/>
+          <SkillList />
+        </dvi>
+      </div>
+    </>
+  );
+  }
+
+function SkillList () {
+  return (
+    <div className="Skill-list">
+      <Skill Skill = "sleep" emoji = "✌️" color = "brown" />
+      <Skill Skill = "play" emoji = "🦾" color = "yellow" />
+      <Skill Skill = "..." emoji = "💀" color = "white" /> 
+      <Skill Skill = "work" emoji = "☠️" color = "silver" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className='Skill' style={{ backgroundColor : props.color }}>
+      <span>{props.Skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>นายวีระยุทธ ลักษ์ศิลป์</h1>
+      <p>
+        นักศึกษาสาขาเทคโนโลยีสารสนเทศและการสื่อสาร คณะวิทยาศาสตร์ 
+        มหาวิทยาลัยอุบลราชธานี
+      </p>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className='avatar' src="avatar.jpg" alt='My Avatar' />;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +59,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
